9bcafc2490e6f65493b5fa99bf209dd030555d36
Karim Aboul-Enein <karim@largelabs.fr>
2022-07-01 17:10:23 +0200

    Interface tutorial

---
+++ Assets/Tutorials/Interfaces.cs
@@ -0,0 +1,93 @@
+﻿
+using UnityEngine;
+
+public class Human : IAgeProvider
+{
+    string personalityDesc = "Cool guy";
+    float height = 2f;
+    int age = 20;
+
+
+    public Human(string i_personality, float i_height, int i_age)
+    {
+        SetAge(i_age);
+    }
+
+    public int Age => age;
+
+    public void SetAge(int i_age)
+    {
+        age = i_age;
+    }
+}
+
+public interface IAgeProvider
+{
+    int Age { get; }
+}
+
+public interface ICharacterStatsProvider : IAgeProvider
+{
+    bool HasWings { get; }
+}
+
+public class AlienDude : ICharacterStatsProvider
+{
+    public bool HasWings => throw new System.NotImplementedException();
+
+    public int Age => throw new System.NotImplementedException();
+}
+
+
+public class HumanCharacter : Human, ICharacterStatsProvider
+{
+    bool hasWings = true;
+
+    public HumanCharacter(bool i_hasWings,string i_personality, float i_height, int i_age) : base(i_personality, i_height, i_age)
+    {
+        SetWings(i_hasWings);
+    }
+
+    public bool HasWings => hasWings;
+
+    public void SetWings(bool i_hasWings)
+    {
+        hasWings = i_hasWings;
+    }
+}
+
+
+public static class CharacterFactory
+{
+    public static ICharacterStatsProvider GetHumanCharacter(bool i_hasWings, string i_personality, float i_height, int i_age)
+    {
+        return new HumanCharacter(i_hasWings,  i_personality,  i_height,  i_age);
+    }
+
+    public static ICharacterStatsProvider GetAlienDude()
+    {
+        return new AlienDude();
+    }
+}
+
+
+// Responsible for printing character stats in a UI
+public class CharacterStatsUI : MonoBehaviour
+{
+    private void Start()
+    {
+        ICharacterStatsProvider myChar = CharacterFactory.GetAlienDude();
+        showUI(myChar);
+
+    }
+
+    void showUI(ICharacterStatsProvider i_char)
+    {
+        // show UI
+    }
+}
+
+
+
+
+
---
+++ Assets/Tutorials/Pools.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+
+// OLD LARGELABS CODE -> use as reference
+
+
+
+/// <summary>
+/// Implement this interface on objects that will be pools in a managed pool
+/// </summary>
+public interface IPoolable : IDisposable
+{
+    void Reset();
+}
+
+
+/// <summary>
+/// Pool that can be used for any type of object that implements IPoolable
+/// </summary>
+/// <typeparam name="T">IPoolable object</typeparam>
+public class ManagedPool<T> : IDisposable where T : class, IPoolable, new()
+{
+    bool isLocked = false;
+
+    // Pool collections
+    List<T> available = new List<T>();
+    Dictionary<T, T> used = new Dictionary<T, T>();
+
+    // CTOR
+    public ManagedPool(int maxCount = -1)   // -1: Unlimited by default
+    {
+        MaxCount = maxCount;
+    }
+
+    #region PUBLIC API
+
+    public int AvailableCount { get { return available.Count; } }
+
+    public int UsedCount { get { return used.Count; } }
+
+    public int TotalCount { get { return AvailableCount + UsedCount; } }
+
+    public int MaxCount { get; private set; }
+
+    public bool IsFull { get { return TotalCount == MaxCount; } }
+
+    public T GetItem()
+    {
+        if (available.Count > 0)
+        {
+            T pooledItem = available[0];
+            available.RemoveAt(0);
+            used.Add(pooledItem, pooledItem);
+
+
+            return pooledItem;
+        }
+
+        if (IsFull) return null;
+
+        T user = new T();
+        used.Add(user, user);
+
+        return user;
+    }
+
+    public void ResetAll()
+    {
+        isLocked = true;
+
+        foreach (KeyValuePair<T, T> pair in used)
+        {
+            pair.Value.Reset();
+            available.Add(pair.Value);
+        }
+
+        used.Clear();
+
+        isLocked = false;
+    }
+
+    public List<T> ResetAll(Func<T, bool> canResetItemFunction)
+    {
+        List<T> removedItems = new List<T>();
+
+        if (canResetItemFunction == null) return removedItems;
+
+        isLocked = true;
+
+
+        foreach (KeyValuePair<T, T> pair in used)
+        {
+            if (canResetItemFunction(pair.Value))
+            {
+                pair.Value.Reset();
+                available.Add(pair.Value);
+
+                removedItems.Add(pair.Value);
+            }
+        }
+
+        for (int i = 0; i < removedItems.Count; i++)
+            used.Remove(removedItems[i]);
+
+        isLocked = false;
+
+        return removedItems;
+    }
+
+    public void Dispose()
+    {
+        isLocked = true;
+
+        foreach (KeyValuePair<T, T> pair in used) pair.Value.Dispose();
+        for (int i = 0; i < available.Count; i++) available[i].Dispose();
+
+        used.Clear();
+        available.Clear();
+
+        isLocked = false;
+    }
+
+    public void DisposeItem(T item)
+    {
+        if (isLocked) return;
+
+        // Processes the item only if it was removed from used.
+        // Otherwise, the object is already available, or does not belong to the pool
+
+        if (used.Remove(item))
+        {
+            item.Dispose();
+        }
+    }
+
+    public void ResetItem(T item)
+    {
+        if (isLocked) return;
+
+        // Processes the item only if it was removed from used.
+        // Otherwise, the object is already available, or does not belong to the pool
+
+        if (used.Remove(item))
+        {
+            item.Reset();
+            if (null == item)
+            {
+                UnityEngine.Debug.LogError("pool item is added null");
+            }
+
+            available.Add(item);
+        }
+    }
+
+    public void Preload(int i_count)
+    {
+        if (i_count < 1)
+            return;
+
+        for (int i = 0; i < i_count; i++)
+        {
+            if (IsFull)
+                return;
+
+            T item = new T();
+            available.Add(item);
+        }
+    }
+
+    #endregion
+}
