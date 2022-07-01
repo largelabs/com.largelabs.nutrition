a35151d8a002d3bed7e4bed7750f69d30364b99f
Karim Aboul-Enein <karim@largelabs.fr>
2022-07-01 16:15:57 +0200

    Code review

--- Assets/Scripts/Animator.cs
+++ Assets/Scripts/Animator.cs
@@ -2,7 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Animator<T>
+public interface IAnimator
+{
+    // Add IsPaused
+
+    bool IsAnimating { get; }
+    float Delay { get; }
+
+    AnimationCurve Function { get; }
+
+    void Pause();
+
+    void Resume();
+
+    float EndTime { get; }
+}
+
+public interface ITypedAnimator<T> : IAnimator
+{
+    T Current { get; }
+
+    T Begin { get; }
+
+    T Target { get; }
+}
+
+
+
+public class Animator<T> : ITypedAnimator<T>
 {
     T begin;
     T end;
@@ -11,15 +38,36 @@ public class Animator<T>
     bool is_animating;
     float delay;
     AnimationCurve function;
+
+    #region CTOR
+
     public Animator(T b, T e, float et, AnimationCurve a, float d=0f){ begin = b; end = e; end_time = et; current = b; is_animating = false; delay = d; function = a; }
+
+    #endregion
+
+    #region IAnimator
+
     public float EndTime => end_time;
-    public T Current => current;
-    public T Begin => begin;
-    public T End => end;
-    public void SetCurrent(T c){ current = c; }
     public bool IsAnimating => is_animating;
     public void Pause() { is_animating = false; }
     public void Resume() { is_animating = true; }
     public float Delay => delay;
     public AnimationCurve Function => function;
+
+    #endregion
+
+    #region ITypedAnimator
+
+    public T Current => current;
+    public T Begin => begin;
+    public T Target => end;
+
+    #endregion
+
+    #region MUTABLE
+
+    public void SetCurrent(T c){ current = c; }
+
+    #endregion
+
 }
--- Assets/Scripts/InterpolatorsManager.cs
+++ Assets/Scripts/InterpolatorsManager.cs
@@ -4,77 +4,49 @@ using UnityEngine;
 
 public class InterpolatorsManager : MonoBehaviour
 {
-    Dictionary<Animator<float>,Coroutine> animators_float = new Dictionary<Animator<float>,Coroutine>();
-    Dictionary<Animator<Vector2>, Coroutine> animators_V2 = new Dictionary<Animator<Vector2>, Coroutine>();
-    Dictionary<Animator<Vector3>, Coroutine> animators_V3 = new Dictionary<Animator<Vector3>, Coroutine>();
-    Dictionary<Animator<Color>, Coroutine> animators_Color = new Dictionary<Animator<Color>, Coroutine>();
-    public Animator<float> Animate(float begin, float end, float time_from_a_to_b,AnimationMode Mode, float Delay_at_the_start = 0)
+    Dictionary<IAnimator, Coroutine> animators = new Dictionary<IAnimator, Coroutine>();
+
+    public ITypedAnimator<float> Animate(float i_start, float i_target, float i_time ,AnimationMode i_interpolationMode, float i_delay = 0)
     {
-        Animator<float> fl = new Animator<float>(begin, end, time_from_a_to_b, Mode.Curve,Delay_at_the_start);
+        Animator<float> fl = new Animator<float>(i_start, i_target, i_time, i_interpolationMode.Curve,i_delay);
         Coroutine coroutine = StartCoroutine(Interpolat(fl));
-        animators_float.Add(fl,coroutine);
+        animators.Add(fl,coroutine);
         return fl;
     }
     public Animator<Vector2> Animate(Vector2 begin, Vector2 end, float time_from_a_to_b, AnimationMode Mode, float Delay_at_the_start = 0)
     {
         Animator<Vector2> fl = new Animator<Vector2>(begin, end, time_from_a_to_b,Mode.Curve, Delay_at_the_start);
         Coroutine coroutine = StartCoroutine(Interpolat(fl));
-        animators_V2.Add(fl, coroutine);
+        animators.Add(fl, coroutine);
         return fl;
     }
     public Animator<Vector3> Animate(Vector3 begin, Vector3 end, float time_from_a_to_b, AnimationMode Mode, float Delay_at_the_start = 0)
     {
         Animator<Vector3> fl = new Animator<Vector3>(begin, end, time_from_a_to_b,Mode.Curve, Delay_at_the_start);
         Coroutine coroutine = StartCoroutine(Interpolat(fl));
-        animators_V3.Add(fl, coroutine);
+        animators.Add(fl, coroutine);
         return fl;
     }
     public Animator<Color> Animate(Color begin, Color end, float time_from_a_to_b, AnimationMode Mode, float Delay_at_the_start = 0)
     {
         Animator<Color> fl = new Animator<Color>(begin, end, time_from_a_to_b,Mode.Curve, Delay_at_the_start);
         Coroutine coroutine = StartCoroutine(Interpolat(fl));
-        animators_Color.Add(fl, coroutine);
+        animators.Add(fl, coroutine);
         return fl;
     }
-    public void Stop(Animator<float> fl)
+    public void Stop(IAnimator fl)
     {
-        if (animators_float.ContainsKey(fl))
+        if (animators.ContainsKey(fl))
         {
-            StopCoroutine(animators_float[fl]);
-            animators_float.Remove(fl);
+            StopCoroutine(animators[fl]);
+            animators.Remove(fl);
         }
     }
-    public void Stop(Animator<Vector2> fl)
-    {
-        if (animators_V2.ContainsKey(fl))
-        {
-            StopCoroutine(animators_V2[fl]);
-            animators_V2.Remove(fl);
-        }
-    }
-    public void Stop(Animator<Vector3> fl)
-    {
-        if (animators_V3.ContainsKey(fl))
-        {
-            StopCoroutine(animators_V3[fl]);
-            animators_V3.Remove(fl);
-        }
-    }
-    public void Stop(Animator<Color> fl)
-    {
-        if (animators_Color.ContainsKey(fl))
-        {
-            StopCoroutine(animators_Color[fl]);
-            animators_Color.Remove(fl);
-        }
-    }
+
     public void StopAllAnimations()
     {
         StopAllCoroutines();
-        animators_float.Clear();
-        animators_V2.Clear();
-        animators_V3.Clear();
-        animators_Color.Clear();
+        animators.Clear();
     }
 
     IEnumerator Interpolat(Animator<float> c)
@@ -84,7 +56,7 @@ public class InterpolatorsManager : MonoBehaviour
         float timer = 0f;
         float end = c.EndTime;
         float a = c.Begin;
-        float b = c.End;
+        float b = c.Target;
         AnimationCurve function = c.Function;
         while (timer < end)
         {
@@ -98,7 +70,7 @@ public class InterpolatorsManager : MonoBehaviour
         }
         c.SetCurrent(Mathf.Lerp(a, b, function.Evaluate(1)));
         c.Pause();
-        animators_float.Remove(c);
+        animators.Remove(c);
     }
 
     IEnumerator Interpolat(Animator<Vector2> c)
@@ -108,7 +80,7 @@ public class InterpolatorsManager : MonoBehaviour
         float timer = 0f;
         float end = c.EndTime;
         Vector2 a = c.Begin;
-        Vector2 b = c.End;
+        Vector2 b = c.Target;
         AnimationCurve function = c.Function;
         while (timer < end)
         {
@@ -122,17 +94,17 @@ public class InterpolatorsManager : MonoBehaviour
         }
         c.SetCurrent(Vector2.Lerp(a, b, function.Evaluate(1)));
         c.Pause();
-        animators_V2.Remove(c);
+        animators.Remove(c);
     }
 
-    IEnumerator Interpolat(Animator<Vector3> c)
+    IEnumerator Interpolat<T>(Animator<Vector3> c)
     {
         yield return new WaitForSeconds(c.Delay);
         c.Resume();
         float timer = 0f;
         float end = c.EndTime;
         Vector3 a = c.Begin;
-        Vector3 b = c.End;
+        Vector3 b = c.Target;
         AnimationCurve function = c.Function;
         while (timer < end)
         {
@@ -146,7 +118,7 @@ public class InterpolatorsManager : MonoBehaviour
         }
         c.SetCurrent(Vector3.Lerp(a, b, function.Evaluate(1)));
         c.Pause();
-        animators_V3.Remove(c);
+        animators.Remove(c);
     }
 
     IEnumerator Interpolat(Animator<Color> c)
@@ -156,7 +128,7 @@ public class InterpolatorsManager : MonoBehaviour
         float timer = 0f;
         float end = c.EndTime;
         Color a = c.Begin;
-        Color b = c.End;
+        Color b = c.Target;
         AnimationCurve function = c.Function;
         while (timer < end)
         {
@@ -170,6 +142,6 @@ public class InterpolatorsManager : MonoBehaviour
         }
         c.SetCurrent(Color.Lerp(a, b, function.Evaluate(1)));
         c.Pause();
-        animators_Color.Remove(c);
+        animators.Remove(c);
     }
 }
