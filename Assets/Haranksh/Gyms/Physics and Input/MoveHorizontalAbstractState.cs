c60f005f861509787fbf64151d0dae52c8581af4
Tarek Mohamed Kassab <tatakassab@gmail.com>
2022-08-09 17:20:16 +0200

    More readable code and better gravity

--- Assets/Haranksh/Gyms/Physics and Input/MoveHorizontalAbstractState.cs
+++ Assets/Haranksh/Gyms/Physics and Input/MoveHorizontalAbstractState.cs
@@ -3,10 +3,48 @@ using UnityEngine;
 
 public abstract class MoveHorizontalAbstractState : State
 {
-    [SerializeField] Controls inputs = null;
-    [SerializeField] float maxVelX = 0f;
+    public PhysicsBody2D body;
+    public float fall_speed;
+    public float steer_speed;
 
-    // Implement horizontal movement
+    protected float originalVelocityX;
+    protected override void onStateEnter()
+    {
+        originalVelocityX = body.VelocityX;
+        controls.MoveStarted += OnSteerPressed;
+        controls.MoveReleased += OnSteerReleased;
+        OnSteerPressed();
+    }
 
+    protected override void onStateExit()
+    {
+        controls.MoveStarted -= OnSteerPressed;
+        controls.MoveReleased -= OnSteerReleased;
+    }
 
+    protected override void onStateFixedUpdate()
+    {
+        body.AddVelocityY(-fall_speed);
+    }
+
+    protected override void onStateInit()
+    {
+    }
+
+    protected override void onStateUpdate()
+    {
+        if (true == body.IsGrounded)
+        {
+            setState<S_idle>();
+            return;
+        }
+    }
+    private void OnSteerPressed()
+    {
+        body.AddVelocityX(steer_speed * controls.MoveDirection());
+    }
+    private void OnSteerReleased()
+    {
+        body.SetVelocityX(originalVelocityX);
+    }
 }
--- Assets/Haranksh/Gyms/Physics and Input/S_fall.cs
+++ Assets/Haranksh/Gyms/Physics and Input/S_fall.cs
@@ -2,66 +2,21 @@ using UnityEngine;
 
 public class S_fall : MoveHorizontalAbstractState
 {
-    [SerializeField] PhysicsBody2D body;
-    [SerializeField] Controls controller;
-    [SerializeField] float fall_gravity;
-    [SerializeField] float strife_velocity_fall;
-
-    private float originalVelocityX;
     protected override void onStateEnter()
     {
-        Debug.Log("Entered Fall");
-        originalVelocityX = body.VelocityX;
-        controller.JumpReleased += OnGlide;
-        controller.MoveStarted += OnSteerPressed;
-        controller.MoveReleased += OnSteerReleased;
-        OnSteerPressed();
-        body.SetGravityModifier(0);
+        Debug.Log("Enter fall");
+        base.onStateEnter();
+        controls.JumpPressed += OnGlide;
     }
 
     protected override void onStateExit()
     {
-        Debug.Log("Exited Fall");
-        controller.JumpReleased -= OnGlide;
-        controller.MoveStarted -= OnSteerPressed;
-        controller.MoveReleased -= OnSteerReleased;
-        OnSteerReleased();
-        body.ResetGravityModifier();
-    }
-
-    protected override void onStateFixedUpdate()
-    {
-        if(true == body.IsGrounded)
-        {
-            setState<S_idle>();
-            return;
-        }
-        body.SetVelocityY(-fall_gravity);
-    }
-
-    protected override void onStateInit()
-    {
-        Debug.Log("Fall State initialized");
-        //put neccessary settings and set variables
-    }
-
-    protected override void onStateUpdate()
-    {
-        //implement falling animation and frame swappers here
-        return;
+        base.onStateExit();
+        controls.JumpPressed -= OnGlide;
     }
 
     private void OnGlide()
     {
         setState<S_glide>();
-    }
-
-    private void OnSteerPressed()
-    {
-        body.AddVelocityX(strife_velocity_fall * controller.MoveDirection());
-    }
-    private void OnSteerReleased()
-    {
-        body.SetVelocityX(originalVelocityX);
     }
 }
