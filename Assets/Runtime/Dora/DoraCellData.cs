f6f9ef5532b7efd8a9217dd3d775c54536e7b60a
Karim Aboul-Enein <karim@largelabs.fr>
2022-08-10 18:50:32 +0200

    DoraCellData implements ISelectable

--- Assets/Runtime/Dora/DoraCellData.cs
+++ Assets/Runtime/Dora/DoraCellData.cs
@@ -1,11 +1,32 @@
 ﻿// NEEDS TO BE POOLED
 using UnityEngine;
 
-public class DoraCellData
+public class DoraCellData : ISelectable
 {
     Transform anchor = null;
     DoraKernel kernel = null;
     Vector2Int coords;
+    bool isSelected = false;
+
+    #region ISelectable
+
+    public bool IsSelected => isSelected;
+
+    public void Select()
+    {
+        if (true == isSelected) return;
+        if (null != kernel) kernel.Select();
+        isSelected = true;
+    }
+
+    public void Unselect()
+    {
+        if (false == isSelected) return;
+        if (null != kernel) kernel.Unselect();
+        isSelected = false;
+    }
+
+    #endregion
 
     public void Init(DoraKernel i_kernel, Transform i_anchor)
     {
