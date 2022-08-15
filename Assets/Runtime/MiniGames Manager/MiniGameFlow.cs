bf8126e49584637081b11b6a2e30c8666bc43691
amr-abdalla <amr_alaa2@aucegypt.edu>
2022-08-16 00:13:24 +0200

    MiniGameFlow implementation

--- Assets/Runtime/MiniGames Manager/MiniGameFlow.cs
+++ Assets/Runtime/MiniGames Manager/MiniGameFlow.cs
@@ -1,20 +1,44 @@
-using System;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public abstract class MiniGameFlow : MonoBehaviourBase
 {
+	protected abstract IEnumerator introRoutine();
+	protected abstract IEnumerator onSuccess();
+	protected abstract IEnumerator onFailure();
+	protected abstract void onGameplayStarted();
+	protected abstract void onGameplayEnded();
+	protected abstract void onGameplayUpdate();
 
-	public abstract void introRoutine();
+	private IEnumerator introSequence()
+	{
+		yield return StartCoroutine(introRoutine());
+		startMiniGame();
+	}
 
-	public abstract void onGameplayStarted();
+	private IEnumerator updateGameplay()
+	{
+		while (true)
+		{
+			onGameplayUpdate();
+			yield return null;
+		}
+	}
 
-	public abstract void onGameplayEnded();
+	private void startMiniGame()
+	{
+		onGameplayStarted();
+		StartCoroutine(updateGameplay());
+	}
 
-	public abstract void onSuccess();
+	public void EnterMiniGame() => StartCoroutine(introSequence());
 
-	public abstract void onFailure();
+	public void EndMiniGame()
+	{
+		StopCoroutine(updateGameplay());
+		onGameplayEnded();
+	}
 
-	public abstract void onExitMiniGame();
-
+	public void ExitMiniGame() => SceneManager.LoadScene(0); //or should we use scene name instead?
 
 }
