ea58133a3f3cb59d06f23e5c500e7905390487b1
Eslam <eslamessam@aucegypt.edu>
2022-08-21 12:58:50 +0200

    Frame Swapper Feature

--- Assets/Runtime/MainMenu/MenuSelector.cs
+++ Assets/Runtime/MainMenu/MenuSelector.cs
@@ -9,62 +9,59 @@ public class MenuSelector : MonoBehaviourBase
     enum MiniGame { Harankash =1 , Dora=2 }
 
     #region Serialized Fields
-    [SerializeField] Controls controls;
-    [SerializeField] MiniGame miniGame;
-    [SerializeField] Transform[] gameNames;
-    [SerializeField] Transform indicator;
+        [SerializeField] Controls controls;
+        [SerializeField] MiniGame miniGame;
+        [SerializeField] Transform[] gameNames;
+        [SerializeField] Transform indicator;
     #endregion
 
     #region Enable/Disable controls
-    private void OnEnable()
-    {
-        controls.MoveStarted += MoveSelector;
-        controls.JumpPressed += SelectGame;
-    }
+        private void OnEnable()
+        {
+            controls.MoveStarted += MoveSelector;
+            controls.JumpPressed += SelectGame;
+        }
 
 
-    private void OnDisable()
-    {
-        controls.MoveStarted -= MoveSelector;
-        controls.JumpPressed -= SelectGame;
-    }
+        private void OnDisable()
+        {
+            controls.MoveStarted -= MoveSelector;
+            controls.JumpPressed -= SelectGame;
+        }
     #endregion
 
     #region Main Methods
-
-    private void MoveSelector()
-    {
-        switch (miniGame)
+        private void MoveSelector()
         {
-            case MiniGame.Harankash:
-                miniGame = MiniGame.Dora;
-                MoveIndicator(gameNames[1]);
-                break;
-            case MiniGame.Dora:
-                miniGame = MiniGame.Harankash;
-                MoveIndicator(gameNames[0]);
-                break;
-            default:
-                break;
+            switch (miniGame)
+            {
+                case MiniGame.Harankash:
+                    miniGame = MiniGame.Dora;
+                    MoveIndicator(gameNames[1]);
+                    break;
+                case MiniGame.Dora:
+                    miniGame = MiniGame.Harankash;
+                    MoveIndicator(gameNames[0]);
+                    break;
+                default:
+                    break;
+            }
         }
-    }
 
-    private void SelectGame()
-    {
-        SelectScene((int)miniGame);
-    }
+        private void SelectGame()
+        {
+            SelectScene((int)miniGame);
+        }
     #endregion
 
     #region Sub-methods
-
-    void MoveIndicator(Transform i_selectedGame)
-    {
-        indicator.position = new Vector3(i_selectedGame.position.x, indicator.position.y, indicator.position.z);
-    }
-    void SelectScene(int i_sceneIndex)
-    {
-        SceneManager.LoadScene(i_sceneIndex);
-    }
-
+        void MoveIndicator(Transform i_selectedGame)
+        {
+            indicator.position = new Vector3(i_selectedGame.position.x, indicator.position.y, indicator.position.z);
+        }
+        void SelectScene(int i_sceneIndex)
+        {
+            SceneManager.LoadScene(i_sceneIndex);
+        }
     #endregion
 }
