66d044c05a5487c78af0f6ca1926535268846b2f
amr-abdalla <amr_alaa2@aucegypt.edu>
2022-08-22 21:43:29 +0200

    Resolving Comments

--- Assets/Runtime/FrameSwapper/IFrameSwapper.cs
+++ Assets/Runtime/FrameSwapper/IFrameSwapper.cs
@@ -1,8 +1,6 @@
 public interface IFrameSwapper
 {
-    // Code review comments : no {get; set;}. Please create a private variable and create a property get for it
-    // and a setter in the mutable public API -> keep this interface read-only
-    float AnimationSpeedMultiplier { get; set; }
+    float AnimationSpeedMultiplier { get; }
 
 	int LoopCount { get; }
 
