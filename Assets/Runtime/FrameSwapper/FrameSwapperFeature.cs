8c8198d21be3af7c173ab7f25811ce363fa7dbcc
Karim Aboul-Enein <karim@largelabs.fr>
2022-08-22 16:56:48 +0200

    code review frame swapper

--- Assets/Runtime/FrameSwapper/FrameSwapperFeature.cs
+++ Assets/Runtime/FrameSwapper/FrameSwapperFeature.cs
@@ -1,12 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class FrameSwapperFeature : StateFeatureAbstract
 {
-    [SerializeField] SpriteFrameSwapper frameSwapper;
+    [SerializeField] SpriteFrameSwapper frameSwapper = null;
+
+    // Code review comments :
+    // Lacks all the options that we might need for such a script.
+    // Adding them below for future implementation
+    [SerializeField] bool playOnStateEnter = true;
+    [SerializeField] bool stopOnStateExit = true;
+    [SerializeField] UnityEvent onStartNewCycle = null; // register to cycle events on the frame swapper and trigger these Unity events
+
     protected override void onStart()
     {
+        // Code review comments :
+        // Don't use the enabled flag, but rather the public API (Play and Stop)
+
+        // Test references to null and log, to avoid NullRefExceptions
         frameSwapper.enabled = true;
     }
     protected override void onExit()
--- Assets/Runtime/FrameSwapper/IFrameSwapper.cs
+++ Assets/Runtime/FrameSwapper/IFrameSwapper.cs
@@ -1,6 +1,8 @@
 public interface IFrameSwapper
 {
-	float AnimationSpeedMultiplier { get; set; }
+    // Code review comments : no {get; set;}. Please create a private variable and create a property get for it
+    // and a setter in the mutable public API -> keep this interface read-only
+    float AnimationSpeedMultiplier { get; set; }
 
 	int LoopCount { get; }
 
