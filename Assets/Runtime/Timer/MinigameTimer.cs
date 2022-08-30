6e3d7c9534521787c99e90e34334ffd19cb5f357
Omar Mohammed Sadek <34074405+omarheoo@users.noreply.github.com>
2022-08-30 13:26:27 +0200

    Basic Countdown Timer API

--- Assets/Runtime/Timer/MinigameTimer.cs
+++ Assets/Runtime/Timer/MinigameTimer.cs
@@ -1,16 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MinigameTimer : MonoBehaviourBase
 {
-    [SerializeField] [Range(10, 500)] private int timeSeconds = 200;
-    [SerializeField] [Range(0f, 1f)] private float timerScale = 1f;
+    [SerializeField] [Range(1, 500)] private float timeSeconds = 200f;
 
-    public delegate void TimerEventDelegate(int i_totalSeconds);
+    enum TimerStatus { Running, Paused, Ended, None };
+    TimerStatus timerStatus = TimerStatus.None;
+
+    public delegate void TimerEventDelegate();
+
     public TimerEventDelegate OnTimerStarted = null;
-    public TimerEventDelegate OnTimerUpdated = null;
     public TimerEventDelegate OnTimerEnded = null;
+    public TimerEventDelegate OnTimerPaused = null;
+    public TimerEventDelegate OnTimerResumed = null;
+
 
     #region UNITY AND CORE
 
@@ -23,7 +29,73 @@ public class MinigameTimer : MonoBehaviourBase
     {
         OnTimerEnded = null;
         OnTimerStarted = null;
-        OnTimerUpdated = null;
+        OnTimerPaused = null;
+        OnTimerResumed = null;
+    }
+
+    private void Update()
+    {
+        updateCountdown();
+    }
+
+    #endregion
+
+    #region PUBLIC API
+
+    [ExposePublicMethod]
+    public void StartTimer()
+    {
+        if (timerStatus != TimerStatus.None) return;
+
+        timerStatus = TimerStatus.Running;
+
+        OnTimerStarted?.Invoke();
+    }
+
+    [ExposePublicMethod]
+    public void PauseTimer()
+    {
+        if (timerStatus == TimerStatus.Paused) return;
+
+        timerStatus = TimerStatus.Paused;
+
+        OnTimerPaused?.Invoke();
+    }
+
+    [ExposePublicMethod]
+    public void ResumeTimer()
+    {
+        if (timerStatus != TimerStatus.Paused) return;
+
+        timerStatus = TimerStatus.Running;
+
+        OnTimerResumed?.Invoke();
+    }
+
+    public string DisplayTimer()
+    {
+
+        return TimeSpan.FromSeconds(timeSeconds).ToString("ss\\.ff");
+    }
+
+    #endregion
+
+    #region PRIVATE API
+
+    private void updateCountdown()
+    {
+        if (timerStatus != TimerStatus.Running) return;
+
+        timeSeconds -= Time.unscaledDeltaTime;
+
+        if (timeSeconds <= 0) onTimeEnd();
+    }
+
+    private void onTimeEnd()
+    {
+        timeSeconds = 0;
+        timerStatus = TimerStatus.Ended;
+        OnTimerEnded?.Invoke();
     }
 
     #endregion
