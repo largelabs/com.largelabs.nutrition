b3d178947056b08e45c7deb808976c0f212f040b
Karim Aboul-Enein <karim@largelabs.fr>
2022-09-07 16:51:17 +0200

    Started the fix for the VFX pool bug. Needs more refactoring

--- Assets/Runtime/Dora/ParticleEffectAutoDespawn.cs
+++ Assets/Runtime/Dora/ParticleEffectAutoDespawn.cs
@@ -6,25 +6,35 @@ public class ParticleEffectAutoDespawn : MonoBehaviour
 {
 	private static readonly string POOL_NAME = "VFX_POOL";
 	SpawnPool pool = null;
+	ParticleSystem ps = null;
 
 	void OnEnable()
 	{
+		ps = GetComponent<ParticleSystem>();
 		pool = PoolManager.Pools[POOL_NAME];
 		StartCoroutine(checkIfAlive());
 	}
 
+    public bool IsAlive => null != ps && ps.IsAlive(true);
+
+	public void Despawn()
+	{
+		pool.Despawn(transform);
+		transform.SetParent(pool.transform);
+	}
+
 	IEnumerator checkIfAlive()
 	{
-		ParticleSystem ps = this.GetComponent<ParticleSystem>();
-
 		while (true && ps != null)
 		{
-			yield return new WaitForSeconds(0.5f);
-			if (!ps.IsAlive(true))
+			if (false == IsAlive)
 			{
-				pool.Despawn(transform);
-				transform.SetParent(pool.transform);
+				Despawn();
+				yield break;
 			}
+
+			yield return null;
+
 		}
 	}
 }
