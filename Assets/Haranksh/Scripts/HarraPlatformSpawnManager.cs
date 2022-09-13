1a54e46e4f6ac56b7511685ea4aa1e4a5c649755
yomozx <37084361+yomozx@users.noreply.github.com>
2022-09-13 15:34:14 +0200

    initial skeleton for spawn system

---
+++ Assets/Haranksh/Scripts/HarraPlatformSpawnManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarraPlatformSpawnManager : MonoBehaviour
+{
+    [SerializeField] private List<AnchorList> possibleSpawnsPerRow = null;
+
+
+}
