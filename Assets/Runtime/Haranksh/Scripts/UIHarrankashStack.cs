3fca9e6904b917013bc81ac7a365d85610a62eb7
yomozx <37084361+yomozx@users.noreply.github.com>
2022-09-20 12:48:45 +0200

    checkpoint on harra UI

--- Assets/Runtime/Haranksh/Scripts/UIHarrankashStack.cs
+++ Assets/Runtime/Haranksh/Scripts/UIHarrankashStack.cs
@@ -10,47 +10,33 @@ public class UIHarrankashStack : UIElementStack<float>
     //[SerializeField] DoraScoreManager scoreManager = null;
     //[SerializeField] DoraSFXProvider sfxProvider = null;
 
+    private Queue<float> queuedHarrankash = null;
     private Stack<UIImageFrameSwapper> uiHarrankashStack = null;
     private Stack<float> scoreStack = null;
+
+    Coroutine stackingRoutine = null;
+
+    #region UNITY
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (stackingRoutine == null)
+            stackingRoutine = StartCoroutine(QueueToStack());
+    }
+
+    private void OnDestroy()
+    {
+        this.DisposeCoroutine(ref stackingRoutine);
+    }
+
+    #endregion
 
     #region PUBLIC API
     public override void CollectUIElements(Queue<float> i_platformScores)
     {
-        if (null == lastAnchor)
-        {
-            lastAnchor = anchorStart;
-        }
-
-        if (null == uiHarrankashStack) uiHarrankashStack = new Stack<UIImageFrameSwapper>();
-        if (null == scoreStack) scoreStack = new Stack<float>();
-
         while (i_platformScores.Count > 0)
-        {
-            scoreStack.Push(i_platformScores.Dequeue());
-            UIImageFrameSwapper uiHarraAnimation = uiHarrankashSpawner.SpawnTransformAtAnchor(lastAnchor, new Vector3(xOffsetPerUIKernel,0, 0), UIHarrankashTypes.Orange);
-
-            RectTransform kernelRect = lastAnchor = uiHarraAnimation.transform as RectTransform;
-
-            kernelRect.SetParent(anchorStart);
-            kernelRect.SetAsFirstSibling();
-
-            Vector3 pos = kernelRect.localPosition;
-            pos.y = (i_platformScores.Count % 2 == 0 ? -2.5f : 2.5f);
-            kernelRect.localPosition = pos;
-
-            uiHarrankashStack.Push(uiHarraAnimation);
-
-            StartCoroutine(
-                scaleRoutine(uiHarraAnimation.transform, interpolatorsManager.Animate(
-                MathConstants.VECTOR_3_ONE,
-                MathConstants.VECTOR_3_ONE * 1.5f,
-                0.5f,
-                new AnimationMode(AnimationType.Bounce))));
-
-        }
-
-        // add option for auto dequeue
-        //if (null == dequeueKernelsRoutine) dequeueKernelsRoutine = StartCoroutine(discardUIElements());
+            queuedHarrankash.Enqueue(i_platformScores.Dequeue());
     }
     #endregion
 
@@ -92,6 +78,49 @@ public class UIHarrankashStack : UIElementStack<float>
     #endregion
 
     #region PRIVATE
+    private IEnumerator QueueToStack()
+    {
+        if (null == lastAnchor)
+        {
+            lastAnchor = anchorStart;
+        }
 
+        if (null == uiHarrankashStack) uiHarrankashStack = new Stack<UIImageFrameSwapper>();
+        if (null == scoreStack) scoreStack = new Stack<float>();
+
+        if (queuedHarrankash == null) queuedHarrankash = new Queue<float>();
+
+        while (true)
+        {
+            while (queuedHarrankash.Count > 0)
+            {
+                scoreStack.Push(queuedHarrankash.Dequeue());
+                UIImageFrameSwapper uiHarraAnimation = uiHarrankashSpawner.SpawnTransformAtAnchor(lastAnchor, new Vector3(xOffsetPerUIKernel, 0, 0), UIHarrankashTypes.Orange);
+
+                RectTransform kernelRect = lastAnchor = uiHarraAnimation.transform as RectTransform;
+
+                kernelRect.SetParent(anchorStart);
+                kernelRect.SetAsFirstSibling();
+
+                uiHarrankashStack.Push(uiHarraAnimation);
+
+                Vector3 pos = kernelRect.localPosition;
+                pos.y = (uiHarrankashStack.Count % 2 == 0 ? -2.5f : 2.5f);
+                kernelRect.localPosition = pos;
+
+                yield return StartCoroutine(
+                    scaleRoutine(uiHarraAnimation.transform, interpolatorsManager.Animate(
+                    MathConstants.VECTOR_3_ONE,
+                    MathConstants.VECTOR_3_ONE * 1.5f,
+                    0.5f,
+                    new AnimationMode(AnimationType.Bounce))));
+            }
+
+            // add option for auto dequeue
+            if (null == dequeueKernelsRoutine) dequeueKernelsRoutine = StartCoroutine(discardUIElements());
+
+            yield return null;
+        }
+    }
     #endregion
 }
