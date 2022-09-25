347ad6d98917babc07f448961fc2da462ab568a9
Omar Mohammed Sadek <34074405+omar-sadek@users.noreply.github.com>
2022-09-25 14:39:59 +0200

    Referencing Inputs in UIHarrankashEndGamePopup

--- Assets/Runtime/Haranksh/Scripts/UI/UIHarrankashEndGamePopup.cs
+++ Assets/Runtime/Haranksh/Scripts/UI/UIHarrankashEndGamePopup.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class UIHarrankashEndGamePopup : UIEndGamePopup
 {
-
+    [SerializeField] private Controls popupInputs = null;
     protected override void enableInputs()
     {
-      
+        popupInputs.EnableControls();
+        popupInputs.OnMoveStarted += onMove;
+        popupInputs.OnMove += onMove;
+        popupInputs.OnEatStarted += onAction;
     }
 
     protected override void disableInputs()
     {
-       
+        popupInputs.DisableControls();
+        popupInputs.OnMoveStarted -= onMove;
+        popupInputs.OnMove -= onMove;
+        popupInputs.OnEatStarted -= onAction;
     }
 
 }
